2c3341bcb826b644f40977d35541ea6c00a9cb60
sharpen <sharpen5500@gmail.com>
2018-01-06 17:40:59 +0900

    예제 코드 추가

--- app-mvp/src/main/java/com/tistory/zxcv5500/app_mvp/view/RepositoryListActivity.java
+++ app-mvp/src/main/java/com/tistory/zxcv5500/app_mvp/view/RepositoryListActivity.java
@@ -10,6 +10,6 @@ public class RepositoryListActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_repository_list);
 	}
 }

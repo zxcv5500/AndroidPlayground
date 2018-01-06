2a14ed7b38dadd4dc61b5c8dbe58041e39217028
zxcv5500 <zxcv5500@gmail.com>
2018-01-06 12:41:34 +0900

    Initial commit

---
+++ app-mvp/src/main/java/com/tistory/zxcv5500/app_mvp/view/RepositoryListActivity.java
@@ -0,0 +1,15 @@
+package com.tistory.zxcv5500.app_mvp.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tistory.zxcv5500.app_mvp.R;
+
+public class RepositoryListActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
